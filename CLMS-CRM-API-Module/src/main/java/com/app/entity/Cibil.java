4d5167a5fd2ba7180a669cd66dcaf71c6f0c133c
Priya-M11 <priyamhanare@gmail.com>
2025-03-25 15:22:03 +0530

    new branch created by priya

--- CLMS-CRM-API-Module/src/main/java/com/app/entity/Cibil.java
+++ CLMS-CRM-API-Module/src/main/java/com/app/entity/Cibil.java
@@ -1,5 +1,39 @@
 package com.app.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.app.enums.CibilStatusEnum;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Cibil {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer cibilId;
+	private Integer cibilScore;
+	@DateTimeFormat
+	private LocalDateTime cibilScoreDateTime;
+	@Enumerated(EnumType.STRING)
+	private CibilStatusEnum status;
+	private String cibilRemark;
+
+
 
 }
