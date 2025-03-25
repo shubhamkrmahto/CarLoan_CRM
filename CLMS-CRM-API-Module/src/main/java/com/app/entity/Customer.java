d67b5cd48ca5bb689ae0e63c7adbb64426a34c9d
Suraj <ingolesuraj53@gmail.com>
2025-03-25 14:00:59 +0530

    all entity classes created with annotations

--- CLMS-CRM-API-Module/src/main/java/com/app/entity/Customer.java
+++ CLMS-CRM-API-Module/src/main/java/com/app/entity/Customer.java
@@ -2,11 +2,19 @@ package com.app.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@NoArgsConstructor
 public class Customer {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer customerId;
 	private String customerName;
 	private LocalDate dateOfBirth;
