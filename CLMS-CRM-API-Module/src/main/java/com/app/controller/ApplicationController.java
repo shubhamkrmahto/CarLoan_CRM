8cb6b5ffd8e72b3108ea61c1fb0e8a6f35784d6e
shubhamkrmahto <skm9258@gmail.com>
2025-05-19 15:35:56 +0530

    updated port of url from resttemplate

--- CLMS-CRM-API-Module/src/main/java/com/app/controller/ApplicationController.java
+++ CLMS-CRM-API-Module/src/main/java/com/app/controller/ApplicationController.java
@@ -49,7 +49,7 @@ public class ApplicationController {
 			@RequestPart("data") String data
 			){
 		
-		String url ="http://localhost:7000/enquiry/getSingleEnquiry/"+id;
+		String url ="http://localhost:9090/crm/enquiry/getSingleEnquiry/"+id;
 		LoanEnquiry enquiry = rt.getForObject(url, LoanEnquiry.class);
 	
 		System.out.println(enquiry);
